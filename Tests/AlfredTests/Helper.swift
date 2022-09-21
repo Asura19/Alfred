b8b534614a0dddc4b6a73659c17ed59fc043ae2e
Phoenix <x.rhythm@qq.com>
2022-09-21 14:17:43 +0800

    Update Helper.swift

--- Tests/AlfredTests/Helper.swift
+++ Tests/AlfredTests/Helper.swift
@@ -2,6 +2,6 @@ import Foundation
 @testable import Alfred
 
 func encodeToString<T: Encodable>(_ value: T) throws -> String {
-    let json = try Response.encoder.encode(value)
+    let json = try Response.makeEncoder().encode(value)
     return String(data: json, encoding: .utf8)!
 }
