ecf32482e672476b2f11ec83f946c65485d9c6d0
Phoenix <x.rhythm@qq.com>
2022-09-21 14:17:11 +0800

    Add Response should sort option.

--- Sources/Alfred/Response.swift
+++ Sources/Alfred/Response.swift
@@ -2,9 +2,11 @@ import Foundation
 
 public struct Response: Encodable {
     var items: [Item]
+    let shouldSort: Bool
 
-    public init(items: [Item] = []) {
+    public init(items: [Item] = [], shouldSort: Bool = true) {
         self.items = items
+        self.shouldSort = shouldSort
     }
 
     public mutating func append(item: ItemRepresentable) {
@@ -15,14 +17,18 @@ public struct Response: Encodable {
         self.items.append(contentsOf: items.map { $0.item })
     }
 
-    public func output() throws -> String {
-        let json = try Self.encoder.encode(self)
+    public mutating func output() throws -> String {
+        let json = try Self.makeEncoder(shouldSort: shouldSort).encode(self)
         return String(data: json, encoding: .utf8)!
     }
 
-    static var encoder: JSONEncoder = {
+    static func makeEncoder(shouldSort: Bool = true) -> JSONEncoder {
         let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+        var formatting: JSONEncoder.OutputFormatting = [.prettyPrinted]
+        if shouldSort {
+            formatting.insert(.sortedKeys)
+        }
+        encoder.outputFormatting = formatting
         return encoder
-    }()
+    }
 }
